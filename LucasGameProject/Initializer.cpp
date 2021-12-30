7b033149530292e6b9b7b764ac4b50a5757a6afb
JasonSatherr <jsather@ucsd.edu>
2021-12-29 20:42:06 -0800

    Contained the code to run on game startup

--- LucasGameProject/Initializer.cpp
+++ LucasGameProject/Initializer.cpp
@@ -1,27 +1,51 @@
 #include "Initializer.h"
 
-namespace startupInit
+namespace start
 {
 	//Creates the window
-	void initWindow(sf::Window* window) {
+	void initWindow(sf::VideoMode* videoMode,sf::RenderWindow ** window) {
+		/**
+			@return void
 
+			Customizes and creates the window
+		*/
+		(videoMode)->width = 800;
+		(videoMode)->height = 600;
+		(*window) = new sf::RenderWindow(*videoMode, "Lucas Game");
+		(*window)->setFramerateLimit(30);
 	}
 	//Starts the music
 	void initMusic(sf::Music* musicPlayer) {
-		if (!musicPlayer->openFromFile("Resources/Music/music1.mp3")) {
+		if (!musicPlayer->openFromFile("Resources/Music/music1.ogg")) {
 			throw ("Music could not be loaded :(");
 		}
+		musicPlayer->setVolume(20);
 		musicPlayer->play();
 		musicPlayer->setLoop(true);
 	}
 	//Loads a Font
-	void initFont(sf::Font* font) {
+	void initFont(sf::Font* font, sf::Text* text) {
 		if (!font->loadFromFile("Resources/Fonts/Shizuru-Regular.ttf")) {
-			throw ("Music could not be loaded :(");
+			throw ("Font could not be loaded :(");
 		}
-		musicPlayer->play();
-		musicPlayer->setLoop(true);
+		//Connect the text to the font
+		initText(font,text);
+		/*musicPlayer->play();
+		musicPlayer->setLoop(true);*/
+	}
+
+	void initText(sf::Font* font, sf::Text* text)
+	{
+		text->setFont(*font);
+		text->setCharacterSize(24);
+		text->setFillColor(sf::Color::Yellow);
 	}
 
 
+	void initAll(sf::VideoMode* videoMode, sf::RenderWindow** window,
+		sf::Music* musicPlayer, sf::Font* font, sf::Text* text) {
+		initWindow(videoMode, window);
+		initMusic(musicPlayer);
+		initFont(font, text);
+	}
 }
--- LucasGameProject/Initializer.h
+++ LucasGameProject/Initializer.h
@@ -5,14 +5,17 @@
 /*
 * Initializes game on boot
 */
-namespace startupInit
+namespace start
 {
 	//Creates the window
-	void initWindow(sf::Window * window);
+	void initWindow(sf::VideoMode* videoMode, sf::Window** window);
 	//Starts the music
 	void initMusic(sf::Music * musicPlayer);
 	//Loads a Font
-	void initFont(sf::Font * font);
+	void initFont(sf::Font* font, sf::Text* text);
+	//Loads the font into the text
+	void initText(sf::Font* font, sf::Text* text);
 
-
+	void initAll(sf::VideoMode* videoMode, sf::RenderWindow** window,
+		sf::Music* musicPlayer, sf::Font* font, sf::Text* text);
 }
