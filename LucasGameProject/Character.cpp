1340196d891c21374f3adbf2fa81ea199e213f99
JasonSatherr <jsather@ucsd.edu>
2021-12-31 00:16:56 -0800

    included virtual update function and implemented commonalities between characters

--- LucasGameProject/Character.cpp
+++ LucasGameProject/Character.cpp
@@ -1,1 +1,33 @@
 #include "Character.h"
+
+void Character::characterInit()
+{
+	this->size.x = 50.0f;
+	this->size.y = 50.0f;
+	this->bodyColor = sf::Color::White;
+	this->position = sf::Vector2f(0.0f, 0.0f);
+	this->body.setSize(this->size);
+	this->body.setFillColor(this->bodyColor);
+	this->body.setPosition(this->position);
+}
+
+void Character::move(float x, float y)
+{
+	this->position.x = this->position.x + x;
+	this->position.y = this->position.y + y;
+	this->body.setPosition(this->position);
+}
+
+Character::Character()
+{
+	characterInit();
+}
+
+Character::~Character()
+{
+}
+
+void Character::draw(sf::RenderWindow* window)
+{
+	window->draw(this->body);
+}
--- LucasGameProject/Character.h
+++ LucasGameProject/Character.h
@@ -1,5 +1,38 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
 class Character
 {
-};
+private:
+	//Setup default characteristics
+	void characterInit();
 
+protected:
+	//Player appearance
+	sf::RectangleShape body;
+	sf::Color bodyColor;
+	sf::Vector2f size;
+	sf::Vector2f position;
+
+	//State properties
+	bool isOnFloor;
+	sf::Color color;
+
+	//Actions
+	//void shoot();
+	void move(float x, float y);
+	//void jump();
+	//void onHit();
+
+
+public:
+
+	Character();
+	virtual ~Character();
+
+	void draw(sf::RenderWindow* window);
+	virtual void updateCharacter() = 0;
+
+
+};
