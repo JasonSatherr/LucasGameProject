72867b93aead623de4bdcb06085a5e7f7da1de33
JasonSatherr <jsather@ucsd.edu>
2022-01-01 20:10:26 -0800

    Added class to calculate position based off dt

--- LucasGameProject/MovementPhysics.cpp
+++ LucasGameProject/MovementPhysics.cpp
@@ -1,1 +1,15 @@
 #include "MovementPhysics.h"
+
+void MovementPhysics::calcPhysics(sf::Vector2f& pos, sf::Vector2f& vel, sf::Vector2f& acc, double dt)
+{
+	/*	Updates position and velocity using delta time, acceleration, 
+	*	position, and velocity
+	*	@returns - void
+	* 
+	*	Perhaps we should change the function name to more precisely describe function.
+	*/
+	vel.x += acc.x * dt;
+	vel.y += acc.y * dt;
+	pos.x += vel.x * dt;
+	pos.y += vel.y * dt;
+}
--- LucasGameProject/MovementPhysics.h
+++ LucasGameProject/MovementPhysics.h
@@ -1,5 +1,14 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 class MovementPhysics
 {
+private:
+	sf::Vector2f position;
+	sf::Vector2f velocity;
+	sf::Vector2f acceleration;
+public:
+	static void calcPhysics(sf::Vector2f& pos, sf::Vector2f & vel, sf::Vector2f & acc, double dt);
+	//Maybe make an overridden function that can 
+	//take a custom defined class that we have that contains all of these
 };
 
