6156e59c5e05e9fa54efbd303e978f9cfbc2050e
JasonSatherr <jsather@ucsd.edu>
2022-01-05 17:42:19 -0800

    Created entity class

---
+++ LucasGameProject/Entity.h
@@ -0,0 +1,13 @@
+#pragma once
+/**
+* The entity abstract class will encompass many game objects
+* 
+* Some examples of entities are trees, projectiles, characters, etc.
+* Anything that could argualy have a collision box should be considered
+* to be an entity
+*/
+class Entity
+{
+
+};
+
