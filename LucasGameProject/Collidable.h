07eae57fa7e21e2f0cdb605e7c49f9ce609ad8ad
JasonSatherr <jsather@ucsd.edu>
2022-01-08 19:26:45 -0800

    add documentation

--- LucasGameProject/Collidable.h
+++ LucasGameProject/Collidable.h
@@ -9,8 +9,21 @@
 */
 class Collidable
 {
+	/**
+	* Will deem if the current collidable entity is colliding with some other...
+	*/
 	virtual bool CollidesWith(Collidable other) = 0;
+	/**
+	* Will handle the collision of this collidable object
+	* 
+	* @param other The other collidable that this object needs to react to.
+	*/
 	virtual void handleCollision(Collidable other) = 0;
+	/**
+	* Will handle the collision the other collidable object
+	* 
+	* @param other The other object that needs to react in the collision.
+	*/
 	virtual void reactToCollision(Collidable other) = 0; //I don't know if this one is required....
 };
 
